0165c1d287590867f6e0a8cd034139d91c48011d
PlutoE <zhangyi20001108@163.com>
2021-11-04 00:19:51 +0800

    Add global namespace definition.

---
+++ include/Utils/def.h
@@ -0,0 +1,17 @@
+/**
+ * This file provides some basic macro definitions.
+ *
+ * @author 19030500131 zy
+ */
+#ifndef DRAWING_LANG_INTERPRETER_DEF_H
+#define DRAWING_LANG_INTERPRETER_DEF_H
+
+/**
+ * definition of namespace begin and end
+ */
+#define INTERPRETER_NAMESPACE_BEGIN namespace drawing {
+#define INTERPRETER_NAMESPACE_END   }
+
+#include <cstddef>
+
+#endif //DRAWING_LANG_INTERPRETER_DEF_H
