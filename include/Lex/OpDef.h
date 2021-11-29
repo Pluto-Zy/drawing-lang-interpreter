777ad25a1d88e37874773493733e9a607624054e
PlutoE <zhangyi20001108@163.com>
2021-11-29 17:53:33 +0800

    `token_kind`: check whether a kind is an operator or keyword

---
+++ include/Lex/OpDef.h
@@ -0,0 +1,18 @@
+/**
+ * This file defines all of the operators in the language.
+ */
+#ifndef op
+#define op(name, spelling)
+#endif
+
+op(semi, ";")
+op(l_paren, "(")
+op(r_paren, ")")
+op(comma, ",")
+op(plus, "+")
+op(minus, "-")
+op(star, "*")
+op(slash, "/")
+op(star_star, "**")
+
+#undef op
